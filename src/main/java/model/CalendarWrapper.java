bf39577e410a91b0e3c467220bd848e38601029f
zhangz22 <michaelzhang295@gmail.com>
2019-09-28 21:43:42 -0400

    update CalendarWrapper

--- src/main/java/model/CalendarWrapper.java
+++ src/main/java/model/CalendarWrapper.java
@@ -23,4 +23,163 @@ public class CalendarWrapper implements Cloneable {
     public CalendarWrapper() {
         this.cal = new GregorianCalendar();
     }
+
+    /**
+     * Constructor.
+     *
+     * @param year         year number
+     * @param month        a number represents the month number. The first month of
+     *                     the year starts from 1 (Jan)
+     * @param day          the day number; starts from 1 to 31
+     * @param hour         the hour number
+     * @param minute       the minute number
+     * @modifies this.cal
+     * @effects create a new CalendarWrapper instance
+     */
+    public CalendarWrapper(final int year, final int month, final int day,
+                           final int hour, final int minute) {
+        this.cal = new GregorianCalendar();
+        this.setYear(year);
+        this.setMonth(month);
+        this.setDate(day);
+        this.setHour(hour);
+        this.setMinute(minute);
+    }
+
+    /**
+     *
+     * This function updates the minute of the time cal is representing
+     *
+     * @param minute the minute number, starts from 0 to 59
+     * @requires None
+     * @modifies cal
+     * @effects update the minute
+     */
+    public void setMinute(int minute) {
+        this.cal.set(Calendar.MINUTE, minute);
+    }
+
+    /**
+     * This function updates the hour of the day of the time cal is representing
+     *
+     * @param hour a number indicating the hour of the day, which is user for the
+     *             24-hour clock. Noon and midnight are represented by 0, not by 12/24.
+     * @requires None
+     * @modifies cal
+     * @effects update the hour
+     */
+    public void setHour(int hour) {
+        this.cal.set(Calendar.HOUR_OF_DAY, hour);
+    }
+
+    /**
+     * This function updates the date of the date cal is representing
+     *
+     * @param date the date number of a month. The first day of the month has value 1.
+     * @requires None
+     * @modifies cal
+     * @effects update the date
+     */
+    public void setDate(int date) {
+        this.cal.set(Calendar.DATE, date);
+    }
+
+    /**
+     * This function updates the month of the date cal is representing
+     *
+     * @param month the month number of a year, starts from 1 (Jan)
+     * @requires None
+     * @modifies cal
+     * @effects update the month
+     */
+    public void setMonth(int month) {
+        this.cal.set(Calendar.MONTH, month-1);
+    }
+
+    /**
+     * This function updates the year of the date cal is representing
+     *
+     * @param year the year number
+     * @requires None
+     * @modifies cal
+     * @effects update the year
+     */
+    public void setYear(int year) {
+        this.cal.set(Calendar.YEAR, year);
+    }
+
+    /**
+     * This function returns the minute of the time cal is representing.
+     *
+     * @requires None
+     * @modifies None
+     * @effects None
+     * @return a number represents current minute. Starts from 0 to 59
+     */
+    public int getMinuteOfHour() {
+        return this.cal.get(Calendar.MINUTE);
+    }
+
+    /**
+     * This function returns the hour of the time cal is representing.
+     *
+     * @requires None
+     * @modifies None
+     * @effects None
+     * @return a number represents current hour. It is used for the 24-hour clock and
+     *         starts from 0 to 23
+     */
+    public int getHourOfDay() {
+        return this.cal.get(Calendar.HOUR_OF_DAY);
+    }
+
+    /**
+     * This function returns the day of the date cal is representing.
+     *
+     * @requires None
+     * @modifies None
+     * @effects None
+     * @return a number represents current date. The first day of the month has value 1.
+     */
+    public int getDay() {
+        return this.cal.get(Calendar.DATE);
+    }
+
+    /**
+     * This function returns the week day of the date cal is representing.
+     *
+     * @requires None
+     * @modifies None
+     * @effects None
+     * @return a number represents current week number. Starts from 0 as Sunday, 1 as Monday...
+     */
+    public int getWeekDay() {
+        return this.cal.get(Calendar.DAY_OF_WEEK) - 1;
+    }
+
+    /**
+     * This function returns the month of the date cal is representing
+     *
+     * @requires None
+     * @modifies None
+     * @effects None
+     * @return the month number of today. The first month of the year starts from 1 (Jan)
+     */
+    public int getMonth() {
+        return this.cal.get(Calendar.MONTH) + 1;
+    }
+
+    /**
+     * This function returns the year of the date cal is representing
+     *
+     * @requires None
+     * @modifies None
+     * @effects None
+     * @return a number represents current year
+     */
+    public int getYear() {
+        return this.cal.get(Calendar.YEAR);
+    }
+
+
 }
